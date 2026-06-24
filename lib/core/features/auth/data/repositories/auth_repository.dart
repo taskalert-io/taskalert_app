dd877d9f04645651a253e9f42027181d71ccf62a
Arnab Banerjee <113848881+ArnabBanerjeehih7@users.noreply.github.com>
2026-06-24 11:37:39 +0530

    Add account type selection in signup

--- lib/core/features/auth/data/repositories/auth_repository.dart
+++ lib/core/features/auth/data/repositories/auth_repository.dart
@@ -24,6 +24,7 @@ abstract class AuthRepository {
     required String password,
     required bool agreeTerms,
     required String otpCode,
+    required String accountType,
     String? email,
     String? gender,
     String? dateOfBirth,
